660a61a6a3ba2a3b64a788ceb0216d0b286f7407
Premanshtomar <premanshotmar@gmail.com>
2022-09-09 00:12:28 +0530

    Changed login screen to a better version.

--- lib/Views/login_view.dart
+++ lib/Views/login_view.dart
@@ -1,9 +1,7 @@
-// import 'package:fluttertoast/fluttertoast.dart';
 // import 'package:firebase_core/firebase_core.dart';
 // import 'package:untitled1/firebase_options.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-// import 'package:fluttertoast/fluttertoast.dart';
 import 'package:untitled1/Views/error_file.dart';
 import 'package:untitled1/services/auth/auth_exceptions.dart';
 import 'package:untitled1/services/auth/auth_services.dart';
@@ -35,92 +33,146 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
+    return  Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+              'asset/login.png'
+          ),fit: BoxFit.cover,
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 12.0),
-        child: Column(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
           children: [
-            TextField(
-              controller: _email,
-              keyboardType: TextInputType.emailAddress,
-              autocorrect: false,
-              enableSuggestions: false,
-              decoration: const InputDecoration(hintText: "example@xyz.com",
-                  border: OutlineInputBorder(),
-                  label: Text("Email")),
+            SingleChildScrollView(
+              child: Container(
+                padding:const EdgeInsets.only(top: 210, left: 40) ,
+                child: const Text('Welcome\n User...',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 55
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height/2,right: 35,left: 35),
+              child:Column(
+                children:[
+                  TextField(
+                    controller: _email,
+                    keyboardType: TextInputType.emailAddress,
+                    autocorrect: false,
+                    enableSuggestions: false,
+                    decoration: InputDecoration(hintText: "example@xyz.com",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
 
-            ),
-            const SizedBox(height: 16,),
-            TextField(
-              controller: _password,
-              obscureText: true,
-              enableSuggestions: false,
-              autocorrect: false,
-              decoration: const InputDecoration(hintText: 'Enter your Password',
-              border: OutlineInputBorder(),
-              label: Text('Password')),
-            ),
-            const SizedBox(height: 16,),
-            TextButton(
-              // style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.grey)),
-              onPressed: () async {
-                final email = _email.text.trim();
-                final password = _password.text;
-                // if (email.isEmpty || password.isEmpty) {
-                //   // Fluttertoast.showToast(
-                //   //   msg: 'Please Add Required Fields',
-                //   //   toastLength: Toast.LENGTH_SHORT,
-                //   //   timeInSecForIosWeb: 1,
-                //   //   textColor: Colors.white,
-                //   //   backgroundColor: Colors.red,
-                //   // );
-                //   showErrorDialog(context, "Please Add Required Fields");
-                //   return;
-                // }
-                // UserCredential? firebaseUser;
-                try {
-                  await AuthServices.firebase().logIn(
-                          email: email, password: password);
-                  if (AuthServices.firebase().currentUser?.isEmailVerified??false){
-                    // ignore: use_build_context_synchronously
-                    Navigator.of(context).pushNamedAndRemoveUntil('/notes/', (route) => false);
-                  }else{
-                    // ignore: use_build_context_synchronously
-                    await AuthServices.firebase().sendEmailVerification();
-                    // ignore: use_build_context_synchronously
-                    Navigator.of(context).pushNamed('/email_verify/');                }
-                } on UserNotFoundAuthException catch (_) {
-                  showErrorDialog(context, 'User Not Found.');
-                }on WrongPasswordAuthException catch (_) {
-                  showErrorDialog(context, 'Wrong Password.');
-                }on InvalidEmailAuthException catch (_) {
-                  showErrorDialog(context, 'Invalid Email');
-                }on GenericAuthException catch (_) {
-                  showErrorDialog(context, 'Authentication Error.');
-                }
+                        ),
+                        fillColor: Colors.grey.shade300,
+                        filled: true,
+                        label: const Text("Email")
+                    ),
 
-                // if (firebaseUser?.user?.uid != null) {}
-                // {
-                //   if (email.isEmpty || password.isEmpty) {
-                //     showErrorDialog(context, 'something went wrong');
-                //     return;
-                //   }
-                // }
-                // print(user.toString());
-              },
-              child: const Text('Login'),
+                  ),
+                  const SizedBox(height: 20,
+                  ),
+
+                  TextField(
+                    controller: _password,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration: InputDecoration(hintText: 'Enter your Password',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15)
+                        ),
+                        fillColor: Colors.grey.shade300,
+                        filled: true,
+                        label: const Text('Password')),
+                  ),
+                  const SizedBox(height: 35,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [const Text('Sign in',
+                      style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w700,
+                        color: Color(0xff4c505b)
+                    ),
+                    ),
+                    CircleAvatar(
+                      radius: 35,
+                      backgroundColor: const Color(0xff4c505b),
+                      child: IconButton(
+                        color: Colors.white,
+                          onPressed: ()
+                            async {
+                              final email = _email.text.trim();
+                              final password = _password.text;
+                              // UserCredential? firebaseUser;
+                              try {
+                                await AuthServices.firebase().logIn(
+                                    email: email, password: password);
+                                if (AuthServices.firebase().currentUser?.isEmailVerified??false){
+                                  // ignore: use_build_context_synchronously
+                                  Navigator.of(context).pushNamedAndRemoveUntil('/notes/', (route) => false);
+                                }else{
+                                  // ignore: use_build_context_synchronously
+                                  await AuthServices.firebase().sendEmailVerification();
+                                  // ignore: use_build_context_synchronously
+                                  Navigator.of(context).pushNamed('/email_verify/');                }
+                              } on UserNotFoundAuthException catch (_) {
+                                showErrorDialog(context, 'User Not Found.');
+                              }on WrongPasswordAuthException catch (_) {
+                                showErrorDialog(context, 'Wrong Password.');
+                              }on InvalidEmailAuthException catch (_) {
+                                showErrorDialog(context, 'Invalid Email');
+                              }on GenericAuthException catch (_) {
+                                showErrorDialog(context, 'Authentication Error.');
+                              }
+                            },
+                          icon: const Icon(Icons.arrow_forward)
+                      ),
+                    ),
+                    ],
+                  ),
+                  const SizedBox(height:100
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(onPressed: () {
+                        Navigator.of(context)
+                            .pushNamedAndRemoveUntil('/register/', (route) => false);
+                      },
+                          child:const Text('Sign up',
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 20,
+                              color: Color(0xff4c505b)
+                            ),
+                          ),
+                      ),
+                      TextButton(onPressed: (){
+
+                      },
+                        child:const Text('Forget Password',
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 20,
+                            color: Color(0xff4c505b)
+                        ),
+                      ),
+                      ),
+                    ],
+                  )
+                ],
+              )
             ),
-            // const SizedBox(height: 16,),
-            TextButton(
-              // style: TextButton.styleFrom(backgroundColor: Colors.grey),
-                onPressed: () {
-                  Navigator.of(context)
-                      .pushNamedAndRemoveUntil('/register/', (route) => false);
-                },
-                child: const Text('Not Registered? Register here!'))
           ],
         ),
       ),
--- lib/services/auth/auth_services.dart
+++ lib/services/auth/auth_services.dart
@@ -1,8 +1,8 @@
-import 'package:untitled1/firebase_options.dart';
+// import 'package:untitled1/firebase_options.dart';
 import 'package:untitled1/services/auth/auth_provider.dart';
 import 'package:untitled1/services/auth/auth_user.dart';
 import 'package:untitled1/services/auth/firebase_auth_provider.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 
 
 class AuthServices implements AuthProvider {
